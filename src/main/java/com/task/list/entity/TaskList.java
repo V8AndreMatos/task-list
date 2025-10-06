6a7e2d848562b2996a9cc168fe894ffca6a78b55
Andre <andreluisv8@gmail.com>
2025-10-06 10:59:41 -0300

    Created class TaskList

---
+++ src/main/java/com/task/list/entity/TaskList.java
@@ -0,0 +1,69 @@
+package com.task.list.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_task")
+public class TaskList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private boolean performed;
+    private int priority;
+
+    public Task() {
+    }
+
+    public Task(Long id, String name, String description, boolean performed, int priority) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.performed = performed;
+        this.priority = priority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isPerformed() {
+        return performed;
+    }
+
+    public void setPerformed(boolean performed) {
+        this.performed = performed;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+}
