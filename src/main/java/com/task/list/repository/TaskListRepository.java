1d6983b3f6cfb1e47678612294d13a34b65c3a3e
Andre <andreluisv8@gmail.com>
2025-10-06 11:02:34 -0300

    Created interface TaskListRepository

---
+++ src/main/java/com/task/list/repository/TaskListRepository.java
@@ -0,0 +1,7 @@
+package com.task.list.repository;
+
+import com.task.list.entity.TaskList;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskListRepository extends JpaRepository<TaskList , Long> {
+}
