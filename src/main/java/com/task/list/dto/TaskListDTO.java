2e369eb8b07ae355ed66fa38f68268642d3ec35c
Andre <andreluisv8@gmail.com>
2025-10-06 11:05:33 -0300

    Created class TaskListDTO

---
+++ src/main/java/com/task/list/dto/TaskListDTO.java
@@ -0,0 +1,62 @@
+package com.task.list.dto;
+
+public class TaskListDTO {
+
+    private Long id;
+
+    private String name;
+    private String description;
+    private boolean performed;
+    private int priority;
+
+    public TaskListDTO() {
+    }
+
+    public TaskListDTO(Long id, String name, String description, boolean performed, int priority) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.performed = performed;
+        this.priority = priority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isPerformed() {
+        return performed;
+    }
+
+    public void setPerformed(boolean performed) {
+        this.performed = performed;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
